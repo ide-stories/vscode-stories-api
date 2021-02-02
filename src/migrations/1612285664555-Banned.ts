230df8499c35cfa04b2b65e176454031dc79f236
Fernando B <fernandobe+git@protonmail.com>
2021-02-02 11:11:55 -0600

    banned migrations

---
+++ src/migrations/1612285664555-Banned.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Banned1612285664555 implements MigrationInterface {
+  name = "Banned1612285664555";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "banned" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "userId" text NOT NULL, "githubId" text NOT NULL, CONSTRAINT "PK_acc9f19a159ce2536a836f51664" PRIMARY KEY ("id"))`
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "isBanned" boolean NOT NULL`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isBanned"`);
+
+    await queryRunner.query(`DROP TABLE "banned"`);
+  }
+}
